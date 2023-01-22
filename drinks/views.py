5706646c384128183085dfada59a2a06cb1e6be0
Quinn Le <quynhnle135@gmail.com>
2023-01-22 07:36:27 -0500

    Implemented GET request

---
+++ drinks/views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Drink
+from .serializers import DrinkSerializer
+
+
+@api_view(['GET', 'POST'])
+def drink_list(request):
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return JsonResponse({'drinks': serializer.data})
+
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
